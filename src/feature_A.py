7c351fe4563c144ae73b0a573acca749e6cbaf37
Sofie <sozp@itu.dk>
2026-03-25 13:14:01 +0100

    added more code to feature A

--- src/feature_A.py
+++ src/feature_A.py
@@ -1,3 +1,19 @@
+def crop(mask):
+        mid = midpointGroup4(mask)
+        y_nonzero, x_nonzero = np.nonzero(mask)
+        y_lims = [np.min(y_nonzero), np.max(y_nonzero)]
+        x_lims = np.array([np.min(x_nonzero), np.max(x_nonzero)])
+        x_dist = max(np.abs(x_lims - mid))
+        x_lims = [mid - x_dist, mid+x_dist]
+        return mask[y_lims[0]:y_lims[1], x_lims[0]:x_lims[1]]
+
+def midpointGroup4(mask):
+        summed = np.sum(mask, axis=0)
+        half_sum = np.sum(summed) / 2
+        for i, n in enumerate(np.add.accumulate(summed)):
+            if n > half_sum:
+                return i
+            
 def get_asymmetry(mask):
     # mask = color.rgb2gray(mask)
     scores = []
